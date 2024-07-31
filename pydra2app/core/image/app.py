62c1b2f8554cb3514240c80cfb4e2135a34bc270
Tom Close <tom.g.close@gmail.com>
2024-07-31 14:48:15 +1000

    fixed up entrypoint in app

--- pydra2app/core/image/app.py
+++ pydra2app/core/image/app.py
@@ -91,7 +91,7 @@ class App(Pydra2AppImage):
 
     def add_entrypoint(self, dockerfile: DockerRenderer, build_dir: Path):
         command_line = (
-            self.command.activate_conda_cmd() + "pydra2app deploy pipeline-entrypoint"
+            self.command.activate_conda_cmd() + "pydra2app pipeline-entrypoint"
         )
 
         dockerfile.entrypoint(shlex.split(command_line))
