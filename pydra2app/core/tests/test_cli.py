da7466b60cf2fb9eaebb9a9c6306dc1e2db1a341
Tom Close <tom.g.close@gmail.com>
2024-08-01 14:53:48 +1000

    fixed up docs reference

--- pydra2app/core/tests/test_cli.py
+++ pydra2app/core/tests/test_cli.py
@@ -342,7 +342,7 @@ a longer description
 ## Command
 |Key|Value|
 |---|-----|
-|Task|frametree.bids.tasks:bids_app|
+|Task|bids:bids_app|
 |Operates on|session|
 #### Inputs
 |Name|Required data-type|Default column data-type|Description|
