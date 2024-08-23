7436905a0f3d215ae6da062899fedfa00fba3664
Tom Close <tom.g.close@gmail.com>
2024-08-23 10:20:45 +1000

    [skip ci] added py3.12 to list of supported versions

--- pipeline2app/core/_version.py
+++ pipeline2app/core/_version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.12.6.dev5+g52181cc"
-__version_tuple__ = version_tuple = (0, 12, 6, "dev5", "g52181cc")
+__version__ = version = "0.13.0"
+__version_tuple__ = version_tuple = (0, 13, 0)
