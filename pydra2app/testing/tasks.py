d390008a088c2cd58b92ec00faeff6d1e0f4b95c
Thomas G. Close <tom.g.close@gmail.com>
2025-09-05 17:10:27 +1000

    debugging unittests

--- pydra2app/testing/tasks.py
+++ pydra2app/testing/tasks.py
@@ -160,7 +160,7 @@ def ContentsAreNumeric(in_file: File) -> bool:
 @python.define(outputs=[LICENSE_OUTPUT_FIELD])
 def CheckLicence(
     expected_license_path: Path,
-    expected_license_contents: str,
+    expected_license_contents: File,
 ) -> File:
     """Checks the `expected_license_path` to see if there is a file with the same contents
     as that of `expected_license_contents`
